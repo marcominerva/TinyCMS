e4e7b72c7ec7b67dc1d9bc9737e98832a9ba78b5
Marco Minerva <marco.minerva@gmail.com>
2022-07-15 15:03:14 +0200

    Add Database project

---
+++ src/TinyCms.Database/Tables/ContentPages.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[ContentPages] (
+    [Id]                   UNIQUEIDENTIFIER CONSTRAINT [DF_ContentPages_Id] DEFAULT (newid()) NOT NULL,
+    [SiteId]               UNIQUEIDENTIFIER NOT NULL,
+    [Title]                NVARCHAR (256)   NOT NULL,
+    [Url]                  NVARCHAR (256)   NOT NULL,
+    [Content]              NVARCHAR (MAX)   NOT NULL,
+    [CreationDate]         DATETIME         CONSTRAINT [DF_ContentPages_CreationDate] DEFAULT (getutcdate()) NOT NULL,
+    [LastModificationDate] DATETIME         NULL,
+    [IsPublished]          BIT              CONSTRAINT [DF_ContentPages_IsPublished] DEFAULT ((1)) NOT NULL,
+    CONSTRAINT [PK_ContentPages] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_ContentPages_Sites] FOREIGN KEY ([SiteId]) REFERENCES [dbo].[Sites] ([Id])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_ContentPages]
+    ON [dbo].[ContentPages]([Url] ASC);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_ContentPages_UniqueUrl]
+    ON [dbo].[ContentPages]([SiteId] ASC, [Url] ASC);
+
---
+++ src/TinyCms.Database/Tables/Sites.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Sites] (
+    [Id]    UNIQUEIDENTIFIER CONSTRAINT [DF_Sites_Id] DEFAULT (newid()) NOT NULL,
+    [Title] NVARCHAR (255)   NOT NULL,
+    CONSTRAINT [PK_Sites] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
