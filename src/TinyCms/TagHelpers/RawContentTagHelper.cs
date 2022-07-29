6e88d8d7bb01af44a2068083609f7bb8a18145f4
Marco Minerva <marco.minerva@gmail.com>
2022-07-29 15:10:02 +0200

    Add Html Sanitize method

---
+++ src/TinyCms/TagHelpers/RawContentTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using TinyCms.Extensions;
+
+namespace TinyCms.TagHelpers;
+
+[HtmlTargetElement("raw")]
+public class RawContentTagHelper : TagHelper
+{
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+        var html = (await output.GetChildContentAsync(NullHtmlEncoder.Default)).GetContent(NullHtmlEncoder.Default);
+        html = HtmlExtensions.Sanitize(html);
+
+        output.TagName = null;
+        output.Content.SetHtmlContent(html);
+
+        await base.ProcessAsync(context, output);
+    }
+}
