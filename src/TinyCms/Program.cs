b96f73df1fbfae470e3553241d8944552df00c98
Marco Minerva <marco.minerva@gmail.com>
2022-08-01 14:52:27 +0200

    Add support for 404 and 403 pages #12 #13

--- src/TinyCms/Program.cs
+++ src/TinyCms/Program.cs
@@ -15,21 +15,22 @@ builder.Services.AddSqlContext(options =>
 builder.Services.AddScoped<IPageService, PageService>();
 
 var app = builder.Build();
+app.UseHttpsRedirection();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+app.UseStatusCodePagesWithReExecute("/Index/{0}");
+
 app.UseStaticFiles();
 
 app.UseRouting();
-
-app.UseAuthorization();
 
 app.MapRazorPages();
 
