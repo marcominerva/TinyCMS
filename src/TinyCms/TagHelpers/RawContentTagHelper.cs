e5a55b8b43630760d159ad8e606c50dee1140181
Andrea Tosato <andrea.tosato@hotmail.it>
2022-10-11 16:15:38 +0200

    EDITOR CONFIG

--- src/TinyCms/TagHelpers/RawContentTagHelper.cs
+++ src/TinyCms/TagHelpers/RawContentTagHelper.cs
@@ -31,37 +31,52 @@ public class RawContentTagHelper : TagHelper
     private static string NormalizeWhiteSpaceText(string text)
     {
         if (string.IsNullOrEmpty(text))
+        {
             return text;
+        }
 
         var lines = GetLines(text);
         if (lines.Length < 1)
+        {
             return text;
+        }
+
 
         string line1 = null;
 
         // find first non-empty line
-        for (int i = 0; i < lines.Length; i++)
+        for (var i = 0; i < lines.Length; i++)
         {
             line1 = lines[i];
             if (!string.IsNullOrEmpty(line1))
+            {
                 break;
+            }
         }
 
         if (string.IsNullOrEmpty(line1))
+        {
             return text;
+        }
 
-        string trimLine = line1.TrimStart();
-        int whitespaceCount = line1.Length - trimLine.Length;
+        var trimLine = line1.TrimStart();
+        var whitespaceCount = line1.Length - trimLine.Length;
         if (whitespaceCount == 0)
+        {
             return text;
+        }
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < lines.Length; i++)
+        var sb = new StringBuilder();
+        for (var i = 0; i < lines.Length; i++)
         {
             if (lines[i].Length > whitespaceCount)
+            {
                 sb.AppendLine(lines[i].Substring(whitespaceCount));
+            }
             else
+            {
                 sb.AppendLine(lines[i]);
+            }
         }
 
         return sb.ToString();
@@ -69,12 +84,16 @@ public class RawContentTagHelper : TagHelper
         string[] GetLines(string s, int maxLines = 0)
         {
             if (s == null)
+            {
                 return null;
+            }
 
             s = s.Replace("\r\n", "\n");
 
             if (maxLines < 1)
+            {
                 return s.Split(new char[] { '\n' });
+            }
 
             return s.Split(new char[] { '\n' }).Take(maxLines).ToArray();
         }
