9ca2a0dd470ca1dcd45060e319ee1b19f258c6ac
Marco Minerva <marco.minerva@gmail.com>
2024-02-05 11:58:31 +0100

    Change database schema

--- database/Tables/Sites.sql
+++ database/Tables/Sites.sql
@@ -1,9 +1,6 @@
 CREATE TABLE [dbo].[Sites] (
     [Id]                UNIQUEIDENTIFIER NOT NULL,
     [Title]             NVARCHAR (255)   NOT NULL,
-    [LogoUrl]           NVARCHAR (255)   NULL,
-    [ShowLogoOnly]      BIT              NOT NULL,
-    [IsPublished]       BIT              NOT NULL,
     [StyleSheetUrls]    NVARCHAR (MAX)   NULL,
     [StyleSheetContent] NVARCHAR (MAX)   NULL
 );
@@ -14,14 +11,10 @@ ALTER TABLE [dbo].[Sites]
 GO
 
 ALTER TABLE [dbo].[Sites]
-    ADD CONSTRAINT [DF_Sites_ShowLogoOnly] DEFAULT ((0)) FOR [ShowLogoOnly];
-GO
-
-ALTER TABLE [dbo].[Sites]
     ADD CONSTRAINT [DF_Sites_Id] DEFAULT (newid()) FOR [Id];
 GO
 
 ALTER TABLE [dbo].[Sites]
-    ADD CONSTRAINT [DF_Sites_IsPublished] DEFAULT ((1)) FOR [IsPublished];
+    ADD CONSTRAINT [DF_Sites_ShowLogoOnly] DEFAULT ((0)) FOR [ShowLogoOnly];
 GO
 
